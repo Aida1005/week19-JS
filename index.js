a8ea1b2245ea04415381b2fce9f3e48bba95297e
Tun Putera Matin <tunputeramatin@TPM-MacBook-Air.local>
2023-06-02 13:04:05 +0800

    correction in js

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//Функция для отображения всех постов через Fetch
+function getPosts (){
+    fetch ("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => {
+        const html = data.reduce((acc, el) => acc + "<div class=\"posts\">" + el.title + "</div>", '');
+        const result = document.querySelector(".post");
+        result.innerHTML = html;
+    })
+}
+
+////Функция для отображения всех постов в браузер
+getPosts();
+
+
